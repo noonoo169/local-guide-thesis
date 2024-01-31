d09b27da7bba243096242cd90df3eec5cc8951ee
vanthinh <ta642379@gmail.com>
2024-01-31 12:25:48 +0700

    feat: config spring security and access token only

--- src/main/java/com/example/localguidebe/repository/UserRepository.java
+++ src/main/java/com/example/localguidebe/repository/UserRepository.java
@@ -2,6 +2,9 @@ package com.example.localguidebe.repository;
 
 import com.example.localguidebe.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface UserRepository extends JpaRepository<User,Integer> {
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+    User findUserByEmail(String email);
 }
