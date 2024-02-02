388e9f26a150220ea3b8fc1f4953748515ff9d2a
vanthinh <ta642379@gmail.com>
2024-02-02 13:53:22 +0700

    feat: allow all origin request

--- src/main/java/com/example/localguidebe/config/AppConfiguration.java
+++ src/main/java/com/example/localguidebe/config/AppConfiguration.java
@@ -4,6 +4,9 @@ import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 public class AppConfiguration {
@@ -17,5 +20,17 @@ public class AppConfiguration {
                 "api_secret", "iB44CBRCuvXp49vjdA6YjjbZ0rM",
                 "secure", true
         ));
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOriginPattern("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsFilter(source);
     }
 }
