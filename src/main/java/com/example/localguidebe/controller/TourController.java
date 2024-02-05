aa18058ef8e5df42159ca9ec7fa2d657f75aaf02
qnguyen <nguyenquan.212002@gmail.com>
2024-02-05 15:25:37 +0700

    feat: get list tour

--- src/main/java/com/example/localguidebe/controller/TourController.java
+++ src/main/java/com/example/localguidebe/controller/TourController.java
@@ -6,8 +6,10 @@ import com.example.localguidebe.dto.responsedto.TourResponseDTO;
 import com.example.localguidebe.entity.Tour;
 import com.example.localguidebe.service.CategoryService;
 import com.example.localguidebe.service.TourService;
+import com.example.localguidebe.system.Result;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -32,11 +34,29 @@ public class TourController {
     @Autowired
     public void setCategoryService(CategoryService categoryService){this.categoryService =categoryService;}
     @PostMapping("/add")
-    public ResponseEntity<TourResponseDTO> addTour(@RequestBody TourRequestDTO tourRequestDTO){
-        Tour newTour = new Tour();
-        BeanUtils.copyProperties(tourRequestDTO, newTour, "categories");
-        tourRequestDTO.getCategories().stream().forEach(category -> newTour.getCategories().add(categoryService.getCategoryById(category.getId())));
-       return ResponseEntity.ok( tourToTourDtoConverter.convert(tourService.saveTour(newTour)));
+    public ResponseEntity<Result> addTour(@RequestBody TourRequestDTO tourRequestDTO){
+        try {
+            Tour newTour = new Tour();
+            BeanUtils.copyProperties(tourRequestDTO, newTour, "categories");
+            tourRequestDTO.getCategories().stream().forEach(category -> newTour.getCategories().add(categoryService.getCategoryById(category.getId())));
+            return new ResponseEntity<>(new Result(true, HttpStatus.OK.value(), "account added successfully",  tourToTourDtoConverter.convert(tourService.saveTour(newTour))), HttpStatus.OK);
+        }catch (Exception e){
+            return new ResponseEntity<>(new Result(false, HttpStatus.CONFLICT.value(), "Adding account failed", null), HttpStatus.CONFLICT);
+        }
+
+    }
+    @GetMapping("/tours")
+    public ResponseEntity<Result> getListTour(){
+        try {
+            List<TourResponseDTO> tourResponseDTOS = new ArrayList<>();
+            List<Tour> tours =tourService.getListTour();
+            for(Tour tour : tours){
+                tourResponseDTOS.add(  tourToTourDtoConverter.convert(tour));
+            }
+            return new ResponseEntity<>(new Result(true, HttpStatus.OK.value(), "Get the list successfully", tourResponseDTOS), HttpStatus.OK);
+        }catch (Exception e){
+            return new ResponseEntity<>(new Result(false, HttpStatus.CONFLICT.value(), "get the failure list", null), HttpStatus.CONFLICT);
+        }
     }
    
 
