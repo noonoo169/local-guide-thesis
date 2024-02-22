b223ed25b06ab33e66272390b18ba5e0abd77860
vanthinh <ta642379@gmail.com>
2024-02-22 11:03:59 +0700

    fix: update-entities-relation

--- src/main/java/com/example/localguidebe/entity/Location.java
+++ src/main/java/com/example/localguidebe/entity/Location.java
@@ -2,12 +2,11 @@ package com.example.localguidebe.entity;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
-import lombok.*;
-
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import lombok.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -24,20 +23,27 @@ public class Location {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
+
   @NotNull
   @Column(name = "name")
   private String name;
+
   @NotNull
   @Column(name = "latitude")
   private String latitude;
+
   @NotNull
   @Column(name = "longitude")
   private String longitude;
+
   @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL, mappedBy = "location")
   private List<Image> images = new ArrayList<>();
 
-  @ManyToMany(mappedBy = "locations",fetch = FetchType.LAZY)
+  @ManyToMany(mappedBy = "locations", fetch = FetchType.LAZY)
   private Set<Tour> tours = new HashSet<>();
+
+  @OneToMany(fetch = FetchType.LAZY, mappedBy = "meetingPoint")
+  private Set<Tour> toursOfMeetingPoint = new HashSet<>();
 
   @Override
   public boolean equals(Object o) {
