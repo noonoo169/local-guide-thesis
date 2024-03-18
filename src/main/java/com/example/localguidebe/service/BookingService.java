15bcbe53092dc1ac76304c7443ac22efe1c52ee9
qnguyen <nguyenquan.212002@gmail.com>
2024-03-18 15:28:50 +0700

    feat: get popular cities

--- src/main/java/com/example/localguidebe/service/BookingService.java
+++ src/main/java/com/example/localguidebe/service/BookingService.java
@@ -1,10 +1,12 @@
 package com.example.localguidebe.service;
 
 import com.example.localguidebe.dto.BookingDTO;
+import com.example.localguidebe.dto.ProvinceResponseDTO;
 
 import java.util.List;
 
 public interface BookingService {
     List<BookingDTO> getBookingHistory(String email);
+    List<ProvinceResponseDTO> FindForSuggestedTours();
 
 }
