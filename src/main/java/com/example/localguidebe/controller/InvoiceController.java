33e418515b270ba24dbcf372f0babdb6347220b3
vanthinh <ta642379@gmail.com>
2024-03-21 16:12:04 +0700

    fix: refund vnpay

--- src/main/java/com/example/localguidebe/controller/InvoiceController.java
+++ src/main/java/com/example/localguidebe/controller/InvoiceController.java
@@ -2,11 +2,17 @@ package com.example.localguidebe.controller;
 
 import com.example.localguidebe.converter.InvoiceToInvoiceDtoConverter;
 import com.example.localguidebe.entity.Invoice;
+import com.example.localguidebe.entity.User;
+import com.example.localguidebe.security.service.CustomUserDetails;
 import com.example.localguidebe.service.InvoiceService;
+import com.example.localguidebe.service.UserService;
 import com.example.localguidebe.system.Result;
+import com.example.localguidebe.utils.AuthUtils;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,11 +23,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class InvoiceController {
   private final InvoiceService invoiceService;
   private final InvoiceToInvoiceDtoConverter invoiceToInvoiceDtoConverter;
+  private final UserService userService;
 
   public InvoiceController(
-      InvoiceService invoiceService, InvoiceToInvoiceDtoConverter invoiceToInvoiceDtoConverter) {
+      InvoiceService invoiceService,
+      InvoiceToInvoiceDtoConverter invoiceToInvoiceDtoConverter,
+      UserService userService) {
     this.invoiceService = invoiceService;
     this.invoiceToInvoiceDtoConverter = invoiceToInvoiceDtoConverter;
+    this.userService = userService;
   }
 
   @GetMapping("/{id}")
@@ -34,12 +44,37 @@ public class InvoiceController {
                     .body(
                         new Result(
                             true,
-                            HttpStatus.NO_CONTENT.value(),
+                            HttpStatus.OK.value(),
                             "Get invoice successfully",
                             invoiceToInvoiceDtoConverter.convert(value))))
         .orElseGet(
             () ->
                 ResponseEntity.status(HttpStatus.NO_CONTENT)
-                    .body(new Result(true, HttpStatus.NO_CONTENT.value(), "Not found invoice")));
+                    .body(new Result(false, HttpStatus.NO_CONTENT.value(), "Not found invoice")));
+  }
+
+  @GetMapping("")
+  public ResponseEntity<Result> getInvoicesOfUser(Authentication authentication) {
+    return AuthUtils.checkAuthentication(
+        authentication,
+        () -> {
+          try {
+            String travelerEmail = ((CustomUserDetails) authentication.getPrincipal()).getEmail();
+            User traveler = userService.findUserByEmail(travelerEmail);
+            List<Invoice> invoices = traveler.getInvoices();
+            return ResponseEntity.status(HttpStatus.OK)
+                .body(
+                    new Result(
+                        true,
+                        HttpStatus.OK.value(),
+                        "Get invoices successfully",
+                        invoices.stream().map(invoiceToInvoiceDtoConverter::convert)));
+          } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(
+                    new Result(
+                        false, HttpStatus.INTERNAL_SERVER_ERROR.value(), "Get invoices failed"));
+          }
+        });
   }
 }
