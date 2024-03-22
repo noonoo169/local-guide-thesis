3eb7b5e4b1d73695f88d8434b43eaf0e25977141
vanthinh <ta642379@gmail.com>
2024-03-22 14:23:45 +0700

    fix: delete traveler request

--- src/main/java/com/example/localguidebe/repository/TravelerRequestRepository.java
+++ src/main/java/com/example/localguidebe/repository/TravelerRequestRepository.java
@@ -3,6 +3,7 @@ package com.example.localguidebe.repository;
 import com.example.localguidebe.entity.TravelerRequest;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -12,4 +13,8 @@ public interface TravelerRequestRepository extends JpaRepository<TravelerRequest
   @Query(
       "SELECT tr FROM TravelerRequest tr JOIN tr.traveler t JOIN tr.guide g WHERE t.email = :email OR g.email = :email")
   List<TravelerRequest> getTravelerRequests(@Param("email") String email);
+
+  @Modifying
+  @Query("delete from TravelerRequest tl where tl.id =:id")
+  void deleteById(@Param("id") Long id);
 }
