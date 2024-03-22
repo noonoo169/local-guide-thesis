e835786555511d7f78057d73d472e3111c9291dc
qnguyen <nguyenquan.212002@gmail.com>
2024-03-22 11:12:40 +0700

    feat: get tour in wishlist

--- src/main/java/com/example/localguidebe/service/WishlistService.java
+++ src/main/java/com/example/localguidebe/service/WishlistService.java
@@ -1,7 +1,11 @@
 package com.example.localguidebe.service;
 
+import com.example.localguidebe.dto.TourDTO;
 import com.example.localguidebe.dto.UserDTO;
+import java.util.List;
 
 public interface WishlistService {
   UserDTO addWishlist(Long tourId, Long travelerId);
+
+  List<TourDTO> getWishlist(Long travelerId);
 }
--- src/main/java/com/example/localguidebe/service/impl/WishlistServiceImpl.java
+++ src/main/java/com/example/localguidebe/service/impl/WishlistServiceImpl.java
@@ -1,12 +1,15 @@
 package com.example.localguidebe.service.impl;
 
+import com.example.localguidebe.converter.TourToTourDtoConverter;
 import com.example.localguidebe.converter.UserToUserDtoConverter;
+import com.example.localguidebe.dto.TourDTO;
 import com.example.localguidebe.dto.UserDTO;
 import com.example.localguidebe.entity.Tour;
 import com.example.localguidebe.entity.User;
 import com.example.localguidebe.repository.TourRepository;
 import com.example.localguidebe.repository.UserRepository;
 import com.example.localguidebe.service.WishlistService;
+import java.util.List;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,14 +17,17 @@ public class WishlistServiceImpl implements WishlistService {
   private final TourRepository tourRepository;
   private final UserRepository userRepository;
   private final UserToUserDtoConverter userToUserDtoConverter;
+  private final TourToTourDtoConverter tourToTourDtoConverter;
 
   public WishlistServiceImpl(
       UserRepository userRepository,
       TourRepository tourRepository,
-      UserToUserDtoConverter userToUserDtoConverter) {
+      UserToUserDtoConverter userToUserDtoConverter,
+      TourToTourDtoConverter tourToTourDtoConverter) {
     this.tourRepository = tourRepository;
     this.userRepository = userRepository;
     this.userToUserDtoConverter = userToUserDtoConverter;
+    this.tourToTourDtoConverter = tourToTourDtoConverter;
   }
 
   @Override
@@ -32,5 +38,12 @@ public class WishlistServiceImpl implements WishlistService {
     tour.getUsers().add(traveler);
     traveler = userRepository.save(traveler);
     return userToUserDtoConverter.convert(traveler);
+  }
+
+  @Override
+  public List<TourDTO> getWishlist(Long travelerId) {
+    User user = userRepository.findById(travelerId).orElseThrow();
+    user.getWishListTour();
+    return user.getWishListTour().stream().map(tourToTourDtoConverter::convert).toList();
   }
 }
