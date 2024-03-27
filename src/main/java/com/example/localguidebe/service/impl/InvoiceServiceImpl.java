d0f9f09c590581d2e8f8d3976d10a9771f8ebc71
vanthinh <ta642379@gmail.com>
2024-03-27 14:01:48 +0700

    fix: send notification after refund

--- src/main/java/com/example/localguidebe/service/impl/InvoiceServiceImpl.java
+++ src/main/java/com/example/localguidebe/service/impl/InvoiceServiceImpl.java
@@ -137,8 +137,21 @@ public class InvoiceServiceImpl implements InvoiceService {
     invoice
         .getBookings()
         .forEach(
-            booking ->
-                busyScheduleService.updateBusyScheduleBeforeUpdateOrDeleteBooking(null, booking));
+            booking -> {
+              busyScheduleService.updateBusyScheduleBeforeUpdateOrDeleteBooking(null, booking);
+
+              // notification send to guide
+              Notification guideNotification =
+                  notificationService.addNotification(
+                      booking.getTour().getGuide().getId(),
+                      invoice.getTraveler().getId(),
+                      booking.getId(),
+                      NotificationTypeEnum.CANCEL_BOOKING,
+                      NotificationMessage.CANCEL_BOOKING);
+              messagingTemplate.convertAndSend(
+                  "/topic/" + booking.getTour().getGuide().getEmail(),
+                  notificationToNotificationDtoConverter.convert(guideNotification));
+            });
     return invoiceRepository.save(invoice);
   }
 
--- src/main/java/com/example/localguidebe/system/NotificationMessage.java
+++ src/main/java/com/example/localguidebe/system/NotificationMessage.java
@@ -7,6 +7,6 @@ public class NotificationMessage {
   public static final String BOOKED_TOUR = "you booked tour successfully";
   public static final String RECEIVED_BOOKING = "you received booking from ";
   public static final String ACCEPTED_TOUR = "your tour is accepted ";
-
   public static final String DENY_TOUR = "your tour is confused";
+  public static final String CANCEL_BOOKING = "your booking has been canceled by ";
 }
