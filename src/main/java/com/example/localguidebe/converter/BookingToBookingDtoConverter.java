2eaa8f750edbec08c992676da77f906227d4f22b
qnguyen <nguyenquan.212002@gmail.com>
2024-03-27 16:38:54 +0700

    fix: get history booking

--- src/main/java/com/example/localguidebe/converter/BookingToBookingDtoConverter.java
+++ src/main/java/com/example/localguidebe/converter/BookingToBookingDtoConverter.java
@@ -6,24 +6,32 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class BookingToBookingDtoConverter {
-  private final TourToTourInBookingResponseDtoConverter tourToTourInBookingResponseDtoConverter;
-  private final UserToGuideDtoConverter userToGuideDtoConverter;
+    private final TourToTourInBookingResponseDtoConverter tourToTourInBookingResponseDtoConverter;
+    private final UserToGuideDtoConverter userToGuideDtoConverter;
+    private final TourDupeToTourDtoConverter tourDupeToTourDtoConverter;
 
-  public BookingToBookingDtoConverter(
-      TourToTourInBookingResponseDtoConverter tourToTourInBookingResponseDtoConverter,
-      UserToGuideDtoConverter userToGuideDtoConverter) {
-    this.tourToTourInBookingResponseDtoConverter = tourToTourInBookingResponseDtoConverter;
-    this.userToGuideDtoConverter = userToGuideDtoConverter;
-  }
+    public BookingToBookingDtoConverter(
+            TourDupeToTourDtoConverter tourDupeToTourDtoConverter,
+            TourToTourInBookingResponseDtoConverter tourToTourInBookingResponseDtoConverter,
+            UserToGuideDtoConverter userToGuideDtoConverter) {
+        this.tourDupeToTourDtoConverter = tourDupeToTourDtoConverter;
+        this.tourToTourInBookingResponseDtoConverter = tourToTourInBookingResponseDtoConverter;
+        this.userToGuideDtoConverter = userToGuideDtoConverter;
+    }
 
-  public BookingDTO convert(Booking source) {
-    return new BookingDTO(
-        source.getId(),
-        source.getStartDate(),
-        source.getNumberTravelers(),
-        source.getPrice(),
-        source.getStatus(),
-        tourToTourInBookingResponseDtoConverter.convert(source.getTour()),
-        userToGuideDtoConverter.convert(source.getTour().getGuide()));
-  }
+    public BookingDTO convert(Booking source) {
+        try {
+            return new BookingDTO(
+                    source.getId(),
+                    source.getStartDate(),
+                    source.getNumberTravelers(),
+                    source.getPrice(),
+                    source.getStatus(),
+                    tourToTourInBookingResponseDtoConverter.convert(source.getTour()),
+                    source.getTourDupe() != null ? tourDupeToTourDtoConverter.convert(source.getTourDupe()) : null,
+                    userToGuideDtoConverter.convert(source.getTour().getGuide()));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
