a63e107b1c7464d7ddda20272c59d2390bb90a13
vanthinh <ta642379@gmail.com>
2024-03-27 17:03:12 +0700

    fix: update user information

--- src/main/java/com/example/localguidebe/utils/JsonUtils.java
+++ src/main/java/com/example/localguidebe/utils/JsonUtils.java
@@ -2,8 +2,12 @@ package com.example.localguidebe.utils;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.beans.PropertyDescriptor;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import org.springframework.beans.BeanWrapper;
+import org.springframework.beans.BeanWrapperImpl;
 
 public class JsonUtils {
   private static final ObjectMapper objectMapper = new ObjectMapper();
@@ -29,5 +33,18 @@ public class JsonUtils {
   public static <T> T convertJsonToObject(String json, TypeReference<T> typeReference)
       throws Exception {
     return objectMapper.readValue(json, typeReference);
+  }
+
+  public static String[] getNullPropertyNames(Object source) {
+    final BeanWrapper src = new BeanWrapperImpl(source);
+    PropertyDescriptor[] pds = src.getPropertyDescriptors();
+
+    Set<String> emptyNames = new HashSet<>();
+    for (PropertyDescriptor pd : pds) {
+      Object srcValue = src.getPropertyValue(pd.getName());
+      if (srcValue == null) emptyNames.add(pd.getName());
+    }
+    String[] result = new String[emptyNames.size()];
+    return emptyNames.toArray(result);
   }
 }
