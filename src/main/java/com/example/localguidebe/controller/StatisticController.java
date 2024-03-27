7d8dbe481b41d7ee1674196519e22302b2507f2f
qnguyen <nguyenquan.212002@gmail.com>
2024-03-27 13:56:33 +0700

    fix: fix statistic api

--- src/main/java/com/example/localguidebe/controller/StatisticController.java
+++ src/main/java/com/example/localguidebe/controller/StatisticController.java
@@ -7,109 +7,103 @@ import com.example.localguidebe.utils.AuthUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/statistic")
 public class StatisticController {
-  private final StatisticService statisticService;
+    private final StatisticService statisticService;
 
-  public StatisticController(StatisticService statisticService) {
-    this.statisticService = statisticService;
-  }
-
-  @GetMapping("/guides")
-  public ResponseEntity<Result> getStatisticByGuides(
-      @RequestParam(required = false, defaultValue = "0") Integer page,
-      @RequestParam(required = false, defaultValue = "5") Integer limit,
-      @RequestParam(required = false, defaultValue = "desc") String order) {
-    try {
-      return new ResponseEntity<>(
-          new Result(
-              false,
-              HttpStatus.OK.value(),
-              "get statistic of guide successfully",
-              statisticService.getStatisticalByGuide(page, limit, order)),
-          HttpStatus.OK);
-    } catch (Exception e) {
-      return new ResponseEntity<>(
-          new Result(false, HttpStatus.CONFLICT.value(), "Get statistics of failed guides", null),
-          HttpStatus.CONFLICT);
+    public StatisticController(StatisticService statisticService) {
+        this.statisticService = statisticService;
     }
-  }
 
-  @GetMapping("/tours")
-  public ResponseEntity<Result> getStatisticByTours(
-      @RequestParam(required = false, defaultValue = "0") Integer page,
-      @RequestParam(required = false, defaultValue = "5") Integer limit,
-      @RequestParam(required = false, defaultValue = "desc") String order) {
-    try {
-      return new ResponseEntity<>(
-          new Result(
-              false,
-              HttpStatus.OK.value(),
-              "get statistic of tour successfully",
-              statisticService.getStatisticalByTour(page, limit, order)),
-          HttpStatus.OK);
-    } catch (Exception e) {
-      return new ResponseEntity<>(
-          new Result(false, HttpStatus.CONFLICT.value(), "Get statistics of failed tours", null),
-          HttpStatus.CONFLICT);
+    @GetMapping("/guides")
+    public ResponseEntity<Result> getStatisticByGuides() {
+        try {
+            return new ResponseEntity<>(
+                    new Result(
+                            false,
+                            HttpStatus.OK.value(),
+                            "get statistic of guide successfully",
+                            statisticService.getStatisticalByGuide()),
+                    HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(
+                    new Result(false, HttpStatus.CONFLICT.value(), "Get statistics of failed guides", null),
+                    HttpStatus.CONFLICT);
+        }
     }
-  }
 
-  @GetMapping("/tour/{TourId}")
-  public ResponseEntity<Result> getStatisticByPerTour(
-      @PathVariable("TourId") Long tourId, Authentication authentication) {
-    return AuthUtils.checkAuthentication(
-        authentication,
-        () -> {
-          try {
+    @GetMapping("/tours")
+    public ResponseEntity<Result> getStatisticByTours() {
+        try {
             return new ResponseEntity<>(
-                new Result(
-                    false,
-                    HttpStatus.OK.value(),
-                    "get statistic of per tour successfully",
-                    statisticService.getStatisticByPerTour(tourId)),
-                HttpStatus.OK);
-          } catch (Exception e) {
+                    new Result(
+                            false,
+                            HttpStatus.OK.value(),
+                            "get statistic of tour successfully",
+                            statisticService.getStatisticalByTour()),
+                    HttpStatus.OK);
+        } catch (Exception e) {
             return new ResponseEntity<>(
-                new Result(
-                    false, HttpStatus.CONFLICT.value(), "Get statistics of failed per tour", null),
-                HttpStatus.CONFLICT);
-          }
-        });
-  }
+                    new Result(false, HttpStatus.CONFLICT.value(), "Get statistics of failed tours", null),
+                    HttpStatus.CONFLICT);
+        }
+    }
 
-  @GetMapping("/guide")
-  public ResponseEntity<Result> getStatisticByPerGuide(Authentication authentication) {
-    return AuthUtils.checkAuthentication(
-        authentication,
-        () -> {
-          try {
-            return new ResponseEntity<>(
-                new Result(
-                    false,
-                    HttpStatus.OK.value(),
-                    "get statistic of per guide successfully",
-                    statisticService.getStatisticByPerGuide(
-                        ((CustomUserDetails) authentication.getPrincipal()).getId())),
-                HttpStatus.OK);
-          } catch (Exception e) {
-            return new ResponseEntity<>(
-                new Result(
-                    false, HttpStatus.CONFLICT.value(), "Get statistics of failed per guide", null),
-                HttpStatus.CONFLICT);
-          }
-        });
-  }
+    @GetMapping("/tour/{TourId}")
+    public ResponseEntity<Result> getStatisticByPerTour(
+            @PathVariable("TourId") Long tourId, Authentication authentication) {
+        return AuthUtils.checkAuthentication(
+                authentication,
+                () -> {
+                    try {
+                        return new ResponseEntity<>(
+                                new Result(
+                                        false,
+                                        HttpStatus.OK.value(),
+                                        "get statistic of per tour successfully",
+                                        statisticService.getStatisticByPerTour(tourId)),
+                                HttpStatus.OK);
+                    } catch (Exception e) {
+                        return new ResponseEntity<>(
+                                new Result(
+                                        false, HttpStatus.CONFLICT.value(), "Get statistics of failed per tour", null),
+                                HttpStatus.CONFLICT);
+                    }
+                });
+    }
 
-  @GetMapping("/guide/tours")
-  public ResponseEntity<Result> getStatisticOfToursByGuide(
-      Authentication authentication,
-      @RequestParam(required = false, defaultValue = "0") Integer page,
-      @RequestParam(required = false, defaultValue = "5") Integer limit,
-      @RequestParam(required = false, defaultValue = "desc") String order) {
+    @GetMapping("/guide")
+    public ResponseEntity<Result> getStatisticByPerGuide(Authentication authentication) {
+        return AuthUtils.checkAuthentication(
+                authentication,
+                () -> {
+                    try {
+                        return new ResponseEntity<>(
+                                new Result(
+                                        false,
+                                        HttpStatus.OK.value(),
+                                        "get statistic of per guide successfully",
+                                        statisticService.getStatisticByPerGuide(
+                                                ((CustomUserDetails) authentication.getPrincipal()).getId())),
+                                HttpStatus.OK);
+                    } catch (Exception e) {
+                        return new ResponseEntity<>(
+                                new Result(
+                                        false, HttpStatus.CONFLICT.value(), "Get statistics of failed per guide", null),
+                                HttpStatus.CONFLICT);
+                    }
+                });
+    }
+
+    @GetMapping("/guide/tours")
+    public ResponseEntity<Result> getStatisticOfToursByGuide(
+            Authentication authentication) {
         return AuthUtils.checkAuthentication(
                 authentication,
                 () -> {
@@ -120,7 +114,7 @@ public class StatisticController {
                                         HttpStatus.OK.value(),
                                         "get statistic of tours by guide successfully",
                                         statisticService.getStatisticOfToursByGuide(
-                                                ((CustomUserDetails) authentication.getPrincipal()).getId(),page,limit,order)),
+                                                ((CustomUserDetails) authentication.getPrincipal()).getId())),
                                 HttpStatus.OK);
                     } catch (Exception e) {
                         return new ResponseEntity<>(
--- src/main/java/com/example/localguidebe/repository/UserRepository.java
+++ src/main/java/com/example/localguidebe/repository/UserRepository.java
@@ -2,20 +2,20 @@ package com.example.localguidebe.repository;
 
 import com.example.localguidebe.entity.User;
 import com.example.localguidebe.enums.RolesEnum;
-import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-  User findUserByEmail(String email);
+    User findUserByEmail(String email);
 
-  Page<User> findAll(Specification<Object> specification, Pageable pageable);
+    Page<User> findAll(Specification<Object> specification, Pageable pageable);
 
-  List<User> findByRoles_Name(RolesEnum roles_name);
+    List<User> findByRoles_Name(RolesEnum roles_name);
 
-  Page<User> findByRoles_Name(RolesEnum roles_name, Pageable pageable);
 }
