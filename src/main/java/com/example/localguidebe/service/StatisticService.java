a0035b4a06f366820d05085eb850c1b6e2899090
qnguyen <nguyenquan.212002@gmail.com>
2024-04-08 10:22:09 +0700

    feat: get the five tour guides with the highest revenue

--- src/main/java/com/example/localguidebe/service/StatisticService.java
+++ src/main/java/com/example/localguidebe/service/StatisticService.java
@@ -2,29 +2,33 @@ package com.example.localguidebe.service;
 
 import com.example.localguidebe.dto.StatisticByMonthDTO;
 import com.example.localguidebe.dto.responsedto.*;
+import java.util.List;
 
 public interface StatisticService {
-    StatisticalGuidePaginationDTO getStatisticalByGuide(Integer page, Integer limit, String order);
+  StatisticalGuidePaginationDTO getStatisticalByGuide(Integer page, Integer limit, String order);
 
-    Double getRevenueByGuide(Long guideId);
+  Double getRevenueByGuide(Long guideId);
 
-    Double getRevenueByTour(Long tourId);
+  Double getRevenueByTour(Long tourId);
 
-    Long getTotalTravelerNumberByTour(Long tourId);
+  Long getTotalTravelerNumberByTour(Long tourId);
 
-    Long getTotalTravelerNumberByGuide(Long guideId);
+  Long getTotalTravelerNumberByGuide(Long guideId);
 
-    StatisticalTourPaginationDTO getStatisticalByTour(Integer page, Integer limit, String order);
+  StatisticalTourPaginationDTO getStatisticalByTour(Integer page, Integer limit, String order);
 
-    Long getTotalBookingByTour(Long tourId);
+  Long getTotalBookingByTour(Long tourId);
 
-    Long getTotalBookingByGuide(Long guideId);
+  Long getTotalBookingByGuide(Long guideId);
 
-    StatisticalTourDTO getStatisticByPerTour(Long tourId);
+  StatisticalTourDTO getStatisticByPerTour(Long tourId);
 
-    StatisticalGuideDTO getStatisticByPerGuide(Long guideId);
+  StatisticalGuideDTO getStatisticByPerGuide(Long guideId);
 
-    StatisticOfToursByGuidePaginationDTO getStatisticOfToursByGuide(
-            Long guideId, Integer page, Integer limit, String order);
-    StatisticByMonthDTO getStatisticByMonth(Integer year);
+  StatisticOfToursByGuidePaginationDTO getStatisticOfToursByGuide(
+      Long guideId, Integer page, Integer limit, String order);
+
+  StatisticByMonthDTO getStatisticByMonth(Integer year);
+
+  List<StatisticalGuideDTO> getTopRevenue();
 }
