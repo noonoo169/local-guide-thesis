a1ab422e20631b9728f26661be1cae74cfaac2c6
Tran Van Thinh <88936808+noonoo169@users.noreply.github.com>
2024-04-25 23:16:07 +0700

    Revert "feat: create repository"
    This reverts commit 0f1ba406ef3ede4c64a97ab1a98da6df90e42208.

--- src/main/java/com/example/localguidebe/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.localguidebe.repository;
-
-import com.example.localguidebe.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Integer> {
-}
