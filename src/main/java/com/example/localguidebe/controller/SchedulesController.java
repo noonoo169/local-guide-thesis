b7dfbedd23636f33b7e61094a600dcae97ea2c0c
Tran Van Thinh <88936808+noonoo169@users.noreply.github.com>
2024-04-25 23:16:07 +0700

    Revert "fix: fix busy schedule"
    This reverts commit 815160abc008eee0d4bfa3e295a0d7bcfc37c9c4.

--- src/main/java/com/example/localguidebe/controller/SchedulesController.java
+++ src/main/java/com/example/localguidebe/controller/SchedulesController.java
@@ -102,7 +102,7 @@ public class SchedulesController {
                     true,
                     HttpStatus.OK.value(),
                     "Get a list of busy schedules and pre booked schedules successfully",
-                    busyScheduleService.getBusyScheduleByGuide(
+                    busyScheduleService.getBusySchedulesAndPreBookedSchedules(
                         ((CustomUserDetails) authentication.getPrincipal()).getEmail())),
                 HttpStatus.OK);
           } catch (Exception e) {
