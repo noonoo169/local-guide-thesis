55d1124b8a5d9c360cc6f43068e5d558c6a3be2f
Tran Van Thinh <88936808+noonoo169@users.noreply.github.com>
2024-04-25 23:16:07 +0700

    Revert "fix: update createBookingInInvoice function"
    This reverts commit df1bb09d0fb75238eaf104f1ac5c0e22e80f2ceb.

--- src/main/java/com/example/localguidebe/service/impl/InvoiceServiceImpl.java
+++ src/main/java/com/example/localguidebe/service/impl/InvoiceServiceImpl.java
@@ -11,6 +11,7 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -34,11 +35,10 @@ public class InvoiceServiceImpl implements InvoiceService {
   @Override
   @Transactional
   public Invoice createBookingInInvoice(List<Long> bookingIds, String email, Double priceTotal) {
+    bookingIds.forEach(bookingRepository::setBookingStatusToPaid);
     Cart cart = cartService.getCartByEmail(email);
     if (cart == null) return null;
-    List<Booking> bookings =
-        new ArrayList<>(
-            cart.getBookings().stream().filter(booking -> !booking.isDeleted()).toList());
+    List<Booking> bookings = new ArrayList<>(cart.getBookings().stream().filter(booking -> !booking.isDeleted()).toList());
     Invoice invoice =
         Invoice.builder()
             .priceTotal(priceTotal)
@@ -50,7 +50,6 @@ public class InvoiceServiceImpl implements InvoiceService {
           booking.setInvoice(invoice);
         });
     invoice.setBookings(bookings);
-    bookingIds.forEach(bookingRepository::setBookingStatusToPaid);
     return invoiceRepository.save(invoice);
   }
 
