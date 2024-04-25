52c42c8bc5981a149db6628b6b4866c9732e7422
Tran Van Thinh <88936808+noonoo169@users.noreply.github.com>
2024-04-25 23:16:07 +0700

    Revert "feat: config spring security and access token only"
    This reverts commit a3c0c0b9cb4bbbb7f1448a8566c82c6b4ea8f94f.

--- src/main/java/com/example/localguidebe/repository/UserRepository.java
+++ src/main/java/com/example/localguidebe/repository/UserRepository.java
@@ -2,9 +2,6 @@ package com.example.localguidebe.repository;
 
 import com.example.localguidebe.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
-public interface UserRepository extends JpaRepository<User, Integer> {
-    User findUserByEmail(String email);
+public interface UserRepository extends JpaRepository<User,Integer> {
 }
