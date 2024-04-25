dc24a9022c4e77259d7a367e3ab3f77f98b82b2c
Tran Van Thinh <88936808+noonoo169@users.noreply.github.com>
2024-04-25 23:16:07 +0700

    Revert "feat: upload delete file function"
    This reverts commit a5e3219ed43dda61a018ae3f7685ef5197205062.

--- src/main/java/com/example/localguidebe/enums/FolderName.java
+++
@@ -1,5 +0,0 @@
-package com.example.localguidebe.enums;
-
-public enum FolderName {
-    people, location, tour
-}
--- src/main/java/com/example/localguidebe/repository/TourStartTimeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.localguidebe.repository;
-
-import com.example.localguidebe.entity.TourStartTime;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TourStartTimeRepository extends JpaRepository<TourStartTime,Integer> {
-}
